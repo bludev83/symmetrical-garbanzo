dcb357a94a7feedd43b02553d5b67f0daeb4f198
bludev83 <bludev83@outlook.com>
2018-06-11 10:46:01 -0400

    minor change - undo spellCheck first attempt

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
-    
+
     timer[0] = Math.floor((timer[3]/100)/60);
     timer[1] = Math.floor((timer[3]/100)- (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
@@ -31,7 +31,6 @@ function spellCheck() {
     let textEntered = testArea.value;
     console.log(textEntered);
 }
-
 // Start the timer once again:
 function start() {
 let textEnteredLength = testArea.value.length;
